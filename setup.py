cf9358e4a0faa1441cdeb49d67cd6a93c2cf1507
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2021-05-29 15:08:30 -0400

    Support automated merging

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="npmp_embed",
     version="1.0.0",
     packages=find_packages(),
-    scripts=['multi_job_embed.sh', 'multi_job_embed_preprocessing.sh'],
+    scripts=['multi_job_embed.sh', 'multi_job_embed_preprocessing.sh', 'embed.sh'],
     entry_points={
         "console_scripts": [
             "dispatch-npmp-embed = dispatch_embed:dispatch_npmp_embed",
@@ -15,6 +15,7 @@ setup(
             "parallel-npmp-preprocessing = mocap_preprocess:parallel_submit",
             "npmp-preprocessing-single-batch = mocap_preprocess:npmp_embed_preprocessing_single_batch",
             "merge-npmp-preprocessing = mocap_preprocess:merge_preprocessed_files",
+            "merge-embed = merge_embed:merge_files",
             "render_training_set_single_batch = generate_training_data:render_training_set_single_batch",
         ]
     },
