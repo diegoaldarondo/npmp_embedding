406da59d79d494c8e0074bc896613bbe81c77d9e
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2021-06-19 18:22:58 -0400

    Change to work with yaml config

--- dispatch_embed_test.py
+++ dispatch_embed_test.py
@@ -3,7 +3,6 @@ from __future__ import division
 from __future__ import print_function
 
 from absl.testing import absltest
-from absl.testing import parameterized
 import os
 import time
 import dispatch_embed
@@ -11,36 +10,40 @@ import dispatch_embed
 
 class DispatchEmbedTest(absltest.TestCase):
     def test_small_job(self):
-        project_folder = "/n/holylfs02/LABS/olveczky_lab/Everyone/dannce_rig/dannce_ephys/art/2020_12_22_2/"
-        os.chdir(project_folder)
-        ref_path = "./npmp_preprocessing/total.hdf5"
-        stac_params = "./stac_params/params.yaml"
-        offset_path = "./stac/offset.p"
-        dataset = "dannce_ephys_" + os.path.basename(os.getcwd())
-        is_lstm = [False, True]
-        is_torque_actuators = [False, True]
-        model_dir = [
-            "/n/holylfs02/LABS/olveczky_lab/Diego/data/dm/comic_models/rodent_tracking_model_21380833_1_final",
-            "/n/holylfs02/LABS/olveczky_lab/Diego/data/dm/comic_models/rodent_tracking_model_24186410_1",
-        ]
-        save_dir = [os.path.join("npmp", os.path.basename(m)) for m in model_dir]
-        for i, (s_dir, model, lstm, torque_actuators) in enumerate(
-            zip(save_dir, model_dir, is_lstm, is_torque_actuators)
-        ):
+        params = dispatch_embed.build_params("test_params.yaml")
+        # project_folder = "/n/holylfs02/LABS/olveczky_lab/Everyone/dannce_rig/dannce_ephys/art/2020_12_22_2/"
+        # os.chdir(project_folder)
+        # ref_path = "./npmp_preprocessing/total.hdf5"
+        # stac_params = "./stac_params/params.yaml"
+        # offset_path = "./stac/offset.p"
+        # dataset = "dannce_ephys_" + os.path.basename(os.getcwd())
+        # is_lstm = [False, True]
+        # is_torque_actuators = [False, True]
+        # model_dir = [
+        #     "/n/holylfs02/LABS/olveczky_lab/Diego/data/dm/comic_models/rodent_tracking_model_21380833_1_final",
+        #     "/n/holylfs02/LABS/olveczky_lab/Diego/data/dm/comic_models/rodent_tracking_model_24186410_1",
+        # ]
+        # save_dir = [os.path.join("npmp", os.path.basename(m)) for m in model_dir]
+        # for i, (s_dir, model, lstm, torque_actuators) in enumerate(
+        #     zip(save_dir, model_dir, is_lstm, is_torque_actuators)
+        # ):
+        #     time.sleep(1)
+        #     batch_file = "_batch_args%d.p" % (i)
+        #     dispatch_embed.dispatch(
+        #         ref_path,
+        #         s_dir,
+        #         dataset,
+        #         model,
+        #         stac_params,
+        #         offset_path,
+        #         lstm,
+        #         torque_actuators,
+        #         batch_file,
+        #         test=True,
+        #     )
+        for batch_params in params:
             time.sleep(1)
-            batch_file = "_batch_args%d.p" % (i)
-            dispatch_embed.dispatch(
-                ref_path,
-                s_dir,
-                dataset,
-                model,
-                stac_params,
-                offset_path,
-                lstm,
-                torque_actuators,
-                batch_file,
-                test=True,
-            )
+            dispatch_embed.dispatch(batch_params)
 
 
 if __name__ == "__main__":
