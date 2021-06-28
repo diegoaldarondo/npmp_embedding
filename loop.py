e6ce47e0a682b8be781b28b468db73a200c00431
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2021-06-27 20:14:06 -0400

    Add multi-sampling loop

--- loop.py
+++ loop.py
@@ -1,11 +1,11 @@
-from dm_control.mujoco.wrapper import mjbindings
 import tensorflow.compat.v1 as tf
 from typing import Dict, Tuple
 import abc
 import observer
 import feeder
+import numpy as np
 
-mjlib = mjbindings.mjlib
+_N_REPEATS_NECESSARY_TO_FREEZE_INPUTS = 10
 
 
 class Loop(abc.ABC):
@@ -65,6 +65,7 @@ class Loop(abc.ABC):
             timestep (TYPE): Timestep object for the current roll out.
             feed_dict (Dict): Dictionary of inputs
         """
+        # TODO: appropriate handling of end of loop without try
         try:
             for n_step in range(self.video_length):
                 print(n_step, flush=True)
@@ -86,9 +87,17 @@ class Loop(abc.ABC):
                 if n_step + 1 == self.video_length:
                     observer.checkpoint(str(self.start_step))
         except IndexError:
-            while len(observer.data["reward"]) < self.video_length:
-                for data_type in observer.data.keys():
-                    observer.data[data_type].append(observer.data[data_type][-1])
+            self.end_loop(observer)
+
+    def end_loop(self, observer: observer.Observer):
+        """Handle the end of the recording.
+
+        Args:
+            observer (observer.Observer): Experiment observer
+        """
+        while len(observer.data["reward"]) < self.video_length:
+            for data_type in observer.data.keys():
+                observer.data[data_type].append(observer.data[data_type][-1])
             # while len(cam_list) < self.video_length:
             #     self.cam_list.append(self.cam_list[-1])
             observer.checkpoint(str(self.start_step))
@@ -116,3 +125,60 @@ class ClosedLoop(Loop):
 class OpenLoop(Loop):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, closed=False)
+
+
+class ClosedLoopMultiSample(ClosedLoop):
+    def __init__(self, *args, n_samples: int = 50, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.n_samples = n_samples
+
+    def loop(
+        self,
+        sess: tf.Session,
+        action_output: Dict,
+        timestep,
+        feed_dict: Dict,
+        observer: observer.Observer = observer.NullObserver(),
+    ):
+        """Roll-out the model in closed loop with the environment.
+
+        Args:
+            sess (tf.Session): Tensorflow session
+            action_output (Dict): Dictionary of logged outputs
+            timestep (TYPE): Timestep object for the current roll out.
+            feed_dict (Dict): Dictionary of inputs
+        """
+        # TODO: appropriate handling of end of loop without try
+        try:
+            for n_step in range(self.video_length):
+                print(n_step, flush=True)
+                observer.grab_frame()
+
+                # Restart at the new step tf the task failed or in open loop
+                if (self.closed and timestep.last()) or not self.closed:
+                    self.env.task.start_step = n_step
+                    timestep, feed_dict = self.reset()
+
+                # Resample n_samples times
+                for n_sample in range(self.n_samples):
+                    action_output_np = sess.run(action_output, feed_dict)
+
+                    # Make observations
+                    observer.observe(action_output_np, timestep)
+
+                # print(
+                #     observer.data["level_1_loc"][10] - observer.data["level_1_loc"][11]
+                # )
+                # # print(observer.data["qpos"][10] - observer.data["qpos"][11])
+                # print(
+                #     observer.data["latent_sample"][10]
+                #     - observer.data["latent_sample"][11]
+                # )
+
+                timestep, feed_dict = self.step(action_output_np)
+
+                # Save a checkpoint of the data and video
+                if n_step + 1 == self.video_length:
+                    observer.checkpoint(str(self.start_step))
+        except IndexError:
+            self.end_loop(observer)
