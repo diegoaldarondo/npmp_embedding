94bdf2f2330eb107a4f0d4fb28a00bf6194a414e
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2021-06-28 22:19:10 -0400

    Refactor and make use less memory

--- merge_embed.py
+++ merge_embed.py
@@ -22,7 +22,7 @@ def get_files(folder: Text) -> List:
 
 
 def merge_files():
-
+    """CLI entrypoint for merging embedding files."""
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -39,39 +39,61 @@ def merge_files():
     )
     args = parser.parse_args()
 
-    # Get the files
+    # Load the files
     log_files = get_files(args.folder)
+    data = load_files(log_files)
 
+    save_merge_file(args.folder, data)
+
+    if args.delete_chunks:
+        for f in log_files:
+            print("deleting", f)
+            os.remove(f)
+
+def load_files(log_files: List) -> Dict:
+    """Load and merge data from all batch files. 
+
+    Args:
+        log_files (List): List of files to load
+
+    Returns:
+        Dict: Merged data
+    """ 
     # Get the first file for the keys
     chunk = loadmat(log_files[0])
-    fields = [field for field in chunk.keys() if '__' not in field]
+    fields = [field for field in chunk.keys() if "__" not in field]
 
     # Load the data
     data = {field: [] for field in fields}
     for f in log_files:
         chunk = loadmat(f)
         for field in fields:
-            data[field].append(chunk[field][:])
+            val = chunk[field][:]
+            if val.dtype == "float" or val.dtype == "float32":
+                val = val.astype("float16").squeeze()
+            data[field].append(val)
 
-    # Concatenate all the chunks and make float16, if possible.
-    data = {field: np.concatenate(data[field]) for field in fields}
-    for field in fields:
-        if (
-            data[field].dtype == "float"
-            or data[field].dtype == "float32"
-        ):
-            data[field] = data[field].astype("float16").squeeze()
+    # Concatenate the data.
+    for field in data.keys():
+        data[field] = np.concatenate(data[field])
+    return data
 
-    with h5py.File(os.path.join(args.folder, "data.hdf5"), "w") as save_file:
+
+def save_merge_file(folder: Text, data: Dict):
+    """Save merged data to file.
+
+    Args:
+        folder (Text): Folder in which to save data.
+        data (Dict): Merged data.
+    """
+    with h5py.File(os.path.join(folder, "data.hdf5"), "w") as save_file:
         for field, v in data.items():
             if field != "action_names":
                 save_file.create_dataset(field, data=v)
             else:
                 names = v[: data["action_mean"].shape[0]]
                 names = [name.split("walker/")[-1].strip() for name in names]
-                save_file.create_dataset("action_names", data=np.array(names, dtype=h5py.string_dtype('utf-8', 30)))
-
-    if args.delete_chunks:
-        for f in log_files:
-            print("deleting", f)
-            os.remove(f)
+                save_file.create_dataset(
+                    "action_names",
+                    data=np.array(names, dtype=h5py.string_dtype("utf-8", 30)),
+                )
