76e97d4caebba340c1646e8305f6cab00dd1ef1d
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2023-06-21 12:53:51 -0400

    switch to unittest

--- dispatch_embed_test.py
+++ dispatch_embed_test.py
@@ -2,13 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from absl.testing import absltest
+import unittest
 import os
 import time
 import dispatch_embed
 
 
-class DispatchEmbedTest(absltest.TestCase):
+class DispatchEmbedTest(unittest.TestCase):
     def test_small_job(self):
         params = dispatch_embed.build_params("test_params.yaml")
         for batch_params in params:
@@ -18,4 +18,4 @@ class DispatchEmbedTest(absltest.TestCase):
 
 
 if __name__ == "__main__":
-    absltest.main()
+    unittest.main()
